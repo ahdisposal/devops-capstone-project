cb76f2b40cda1b04ea070f36c6c959b4608d7d30
ahdisposal <96834940+ahdisposal@users.noreply.github.com>
2022-12-07 12:49:54 +0100

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -39,6 +39,7 @@ def index():
 ######################################################################
 
 @app.route("/accounts", methods=["POST"])
+
 def create_accounts():
     """
     Creates an Account
@@ -62,6 +63,7 @@ def create_accounts():
 ######################################################################
 
 @app.route("/accounts", methods=["GET"])
+
 def list_accounts():
     """
     List all Accounts
@@ -78,6 +80,7 @@ def list_accounts():
 ######################################################################
 
 @app.route("/accounts/<int:account_id>", methods=["GET"])
+
 def get_accounts(account_id):
     """
     Reads an Account
@@ -96,6 +99,7 @@ def get_accounts(account_id):
 ######################################################################
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
+
 def update_accounts(account_id):
     """
     Update an Account
@@ -117,6 +121,7 @@ def update_accounts(account_id):
 ######################################################################
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
+
 def delete_accounts(account_id):
     """
     Delete an Account
