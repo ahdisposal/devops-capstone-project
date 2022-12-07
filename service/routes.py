3bad20ebb0a4964333075a24e706045e6e65197b
ahdisposal <96834940+ahdisposal@users.noreply.github.com>
2022-12-07 13:23:19 +0100

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -59,9 +59,7 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-
 @app.route("/accounts", methods=["GET"])
-
 def list_accounts():
     """
     List all Accounts
@@ -76,9 +74,7 @@ def list_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["GET"])
-
 def get_accounts(account_id):
     """
     Reads an Account
@@ -95,9 +91,7 @@ def get_accounts(account_id):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
-
 def update_accounts(account_id):
     """
     Update an Account
@@ -117,9 +111,7 @@ def update_accounts(account_id):
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
-
 def delete_accounts(account_id):
     """
     Delete an Account
